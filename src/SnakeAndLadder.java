5d0479007295511059432957f5d0fa09d2204400
unknown <papisettynagendra>
2022-08-04 14:44:53 +0530

    added commit one

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,5 +1,11 @@
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Snake And Ladder");
+        //int position = 0;
+        System.out.println(" Player Rolls the Die");
+
+        int random = (int) Math.floor(Math.random()* 10) % 6 +1;
+
+
+        System.out.println(" Number Got: " + random);
     }
 }
